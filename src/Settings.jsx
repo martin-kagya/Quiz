915423e98980dfb29edadbf6075df2cfa99c501a
martin-kagya <mkagyemang2@st.knust.edu.gh>
2023-10-16 15:33:13 +0000

    experimenting with question display

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './index.css'
 import {useState, useEffect} from 'react'
 import questions from './questions.json'
 
@@ -11,12 +12,22 @@ function Settings ()
   {
     setCategory(e.target.value)
   }
+  function handleSubmit()
+  {
+    if(category === 'History')
+    return(
+      <div>
+        <h2>{questions[0].questions[0].question}</h2>
+      </div>
+    )
+  } 
   return (
     <>
-      <div>
+      <div className='container'>
         <h1>
           Quiz App
         </h1>
+      <form>
         <select value={category} onChange={handleCatChange}>
           <option value="">Select a category</option>
             {
@@ -28,6 +39,8 @@ function Settings ()
               
             ))}
         </select>
+        <button onSubmit={handleSubmit}>Next</button>
+      </form>
       </div>
     </>
   )
